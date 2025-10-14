0e4f3482d252e795ffd99085c2a1a34c5ebb7323
XSERGIODM <sergiodanilomosqueraquigua@gmail.com>
2025-10-14 04:18:21 -0500

    Implementa página de detalle de personaje
    Crea la vista detallada para personajes de Los Simpson.
    
    La página incluye un encabezado temático, una tarjeta 3D interactiva para el retrato y datos clave, un panel lateral con información extendida, frases célebres y acciones de navegación.
    
    La lógica del componente obtiene los datos del personaje a través de la ruta activa y gestiona la visualización de imágenes. Se añaden estilos específicos para el efecto 3D y el diseño general.

--- src/app/theSimpsons/page/destalle-personaje-page/destalle-personaje-page.ts
+++ src/app/theSimpsons/page/destalle-personaje-page/destalle-personaje-page.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import {Component, inject, input, OnInit, signal} from '@angular/core';
+import {PersonajeType} from '../../type/PersonajeType';
+import {environment} from '../../../../environments/environment';
+import {NgOptimizedImage} from '@angular/common';
+import {ActivatedRoute, RouterLink} from '@angular/router';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {PersonajeService} from '../../service/personaje-service';
+import {map} from 'rxjs';
 
 @Component({
   selector: 'app-destalle-personaje-page',
-  imports: [],
-  templateUrl: './destalle-personaje-page.html'
+  imports: [
+    NgOptimizedImage,
+    RouterLink
+  ],
+  templateUrl: './destalle-personaje-page.html',
+  styleUrl: './destalle-personaje.css'
 })
-export default class DestallePersonajePage {
+export default class DestallePersonajePage implements OnInit {
+
+  personaje = signal<PersonajeType>({
+    id: 0,
+    name: '',
+    occupation: '',
+    age: 0,
+    status: '',
+    gender: '',
+    birthdate: new Date(),
+    portrait_path: '',
+    phrases: []
+  })
+
+  urlImagenBase = signal<string>(environment.apiImagenUrl)
+
+  personajeService = inject(PersonajeService);
+
+  rutaActiva = toSignal(
+    inject(ActivatedRoute)
+      .params
+      .pipe(
+        map(
+          (parametro) =>
+            parametro['id']
+        )
+      )
+  );
+
+  ngOnInit(): void {
+    this.personajeService.obtenerPersonajePorId(this.rutaActiva()).subscribe(data => {
+      this.personaje.set(data);
+    });
+  }
+
+
+  obtenerImagen(size:number, path:string): String{
+    return this.urlImagenBase()+'/'+size +path;
+  }
 
 }
